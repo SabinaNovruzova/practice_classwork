0c3d7ec92a57e7db1a9b5aa432c8da8d594c3114
Sabina Novruzova <tu7cldm19@aztu.local>
2022-03-31 17:04:55 +0400

    first commit

---
+++ practice/practice/Program.cs
@@ -0,0 +1,17 @@
+﻿using practice.Models;
+using System;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu = new Student("Novruzova Sebine",700);
+            stu.StudentInfo();
+            Console.WriteLine("------------------------------------");
+            User us = new User("Novruzova Sebine", "sebine@gmail.com", "sebine123");
+            us.ShowInfo();
+        }
+    }
+}
